8ed15bda57c4eeedd1140cc3259a08bed0c3b121
Scott Erickson <sderickson@gmail.com>
2018-03-22 14:41:01 -0700

    Build initial landing page

---
+++ app/views/teachers/APCSPLanding.vue
@@ -0,0 +1,99 @@
+<template lang="jade">
+  #apcsp-landing
+    h1.text-center 
+      | Teaching AP <sup>®</sup> Computer Science Principles? 
+    h2.text-center We’ve got you covered.
+  
+    p Are you teaching AP<sup>®</sup> Computer Science Principles at your high school for the 2018-2019 school year? CodeCombat’s comprehensive curriculum and professional development program are all you need to offer College Board’s newest computer science course to your students.  
+
+    p AP<sup>®</sup> Computer Science Principles spotlights how computing is changing the world, and provides ample opportunity for students from all backgrounds to connect abstract concepts to real-world implications of the field. Teachers can use our AP<sup>®</sup> CSP Curriculum and Professional Development Hub as their primary resource for teaching the course and preparing students for the AP<sup>®</sup> exam. 
+
+    p We’ve designed our resources to support teachers regardless of their programming experience, enabling every educator to facilitate this course with confidence. Our team at CodeCombat will be with you every step of the way.
+  
+    h3 CodeCombat’s curriculum features:
+    ul
+      li Free, self-paced professional development with guidance from our in-house educational experts
+      li Guidance on all aspects of the course, including the AP<sup>®</sup> CSP Performance Tasks and how to utilize practice opportunities to set your students up for success.
+      li Real, typed code in either JavaScript or Python to support your students’ creativity
+
+    #endorsement
+      img#ap-provider-badge(src="/images/pages/apcsp/APCSP_ProviderBadge_lg.png")
+      div CodeCombat is recognized by the College Board as an endorsed provider of curriculum and professional development for AP<sup>®</sup> Computer Science Principles (AP CSP). This endorsement affirms that all components of CodeCombat‘s offerings are aligned to the AP Curriculum Framework standards and the AP CSP assessment. Using an endorsed provider affords schools access to resources including an AP CSP syllabus pre-approved by the College Board’s AP Course Audit, and officially recognized professional development that prepares teachers to teach AP CSP.
+    
+  
+    p.text-center
+      strong
+        | Email
+        =" "
+        a(mailto="jane@codecombat.com") jane@codecombat.com
+        =" "
+        | for details
+      
+    hr
+  
+    h3 Requirements
+  
+    h5 Programming Languages
+    p CodeCombat’s course can be used to learn programming in either JavaScript or Python, which teachers will choose upon creation of their classroom inside the CodeCombat platform.
+  
+    h5 Teacher Verification
+    p Teachers who apply for and are approved through CodeCombat’s verification process will be given access to our full AP® Computer Science Principles curriculum, including all professional development and assessment materials. The verification process does not cost teachers anything, but is designed to maximize our professional development efforts.
+  
+    h5 Minimum Hardware/Software Specifications
+    p CodeCombat runs best on computers with at least 4GB of RAM, on a modern browser such as Chrome, Safari, Firefox, or Edge. Chromebooks with 2GB of RAM may have minor graphics issues in courses beyond Computer Science 3, though there should be minimal issues with the recommended content for AP® Computer Science Principles as outlined. A minimum of 200 Kbps bandwidth per student is required, although 1+ Mbps is recommended.
+  
+    h5 Professional Development
+    p Our professional development materials are made available to all verified teachers at no cost. Professional development is self-directed and self-paced, taking place online and in collaborative forums where teachers can ask questions and participate in discussions with other verified teachers and CodeCombat content experts. We encourage all teachers to go through professional development before using CodeCombat as their AP® Computer Science Principles curriculum, and we provide support throughout the year as their classes progress through the curriculum.
+  
+    h5 Student Licenses
+    p In order for teachers to be able to assign the required CodeCombat courses to students in their class, each student will need a License. Information on license pricing and structure can be obtained by speaking to CodeCombat’s school specialists (email schools@codecombat.com). We recommend that licenses are obtained by early August of the coming school year so students can begin as early as the first day of the fall semester.
+  
+    p
+      i AP<sup>®</sup> and Advanced Placement<sup>®</sup> are registered trademarks of the College Board. Used with permission.
+
+
+</template>
+
+<script lang="coffee">
+  module.exports = Vue.extend({
+    data: -> {}
+
+    created: ->
+      
+  })
+
+</script>
+
+<style lang="sass">
+  #apcsp-landing
+    max-width: 800px
+    margin: 0 auto
+    
+    h1
+      margin: 30px 0 10px
+      
+    h2
+      margin-bottom: 40px
+      
+    h3
+      margin: 30px 0 10px
+      
+    h5
+      margin: 30px 0 5px
+      
+    ul
+      margin: 20px 0
+      
+    p
+      margin: 0 0 25px
+      
+    #endorsement
+      margin: 50px 0
+      font-style: italic
+      display: flex
+
+    #ap-provider-badge
+      width: 125px
+      margin-right: 30px
+      align-self: center
+</style>
